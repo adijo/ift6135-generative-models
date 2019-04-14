e03abadcf00796b6f673e0945532e1c652f2129a
Aditya Joshi <1adityajoshi@gmail.com>
2019-04-14 15:51:24 -0400

    Add basic VAE code

---
+++ Problem3/vae/train.py
@@ -0,0 +1,50 @@
+from torch import optim
+from torch.autograd import Variable
+import torch
+
+from Problem3.vae.svhn import get_data_loader
+from Problem3.vae.vae import VAE, loss_function
+
+
+def train(model, optimizer, train_loader, loss_function, epoch, use_cuda=True, log_interval=10):
+    # toggle model to train mode
+    model.train()
+    train_loss = 0
+    # in the case of MNIST, len(train_loader.dataset) is 60000
+    # each `data` is of BATCH_SIZE samples and has shape [128, 1, 28, 28]
+    for batch_idx, (data, _) in enumerate(train_loader):
+        data = Variable(data)
+        if use_cuda:
+            data = data.cuda()
+        optimizer.zero_grad()
+
+        # push whole batch of data through VAE.forward() to get recon_loss
+        recon_batch, mu, log_var = model(data)
+        # calculate scalar loss
+        loss = loss_function(recon_batch, data, mu, log_var)
+
+        loss.backward()
+        train_loss += loss.data[0]
+        optimizer.step()
+        if batch_idx % log_interval == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader),
+                loss.data[0] / len(data)))
+
+    print('====> Epoch: {} Average loss: {:.4f}'.format(
+          epoch, train_loss / len(train_loader.dataset)))
+
+
+Z_DIMS = 100
+NUM_EPOCHS = 1
+BATCH_SIZE=64
+is_cuda = torch.cuda.is_available()
+model = VAE(z_dims=Z_DIMS)
+if is_cuda:
+    model = model.cuda()
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
+train_loader, valid_loader, test_loader = get_data_loader("svhn", BATCH_SIZE)
+
+for epoch in range(NUM_EPOCHS):
+    train(model, optimizer, train_loader, loss_function, epoch)
---
+++ Problem3/vae/vae.py
@@ -0,0 +1,130 @@
+import torch
+
+from torch import nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+
+
+class VAE(nn.Module):
+    def __init__(self, z_dims):
+        super(VAE, self).__init__()
+
+        # ENCODER
+        # 28 x 28 pixels = 784 input pixels, 400 outputs
+        self.fc1 = nn.Linear(784, 400)
+        # rectified linear unit layer from 400 to 400
+        # max(0, x)
+        self.relu = nn.ReLU()
+        self.fc21 = nn.Linear(400, z_dims)  # mu layer
+        self.fc22 = nn.Linear(400, z_dims)  # logvariance layer
+        # this last layer bottlenecks through ZDIMS connections
+
+        # DECODER
+        # from bottleneck to hidden 400
+        self.fc3 = nn.Linear(z_dims, 400)
+        # from hidden 400 to 784 outputs
+        self.fc4 = nn.Linear(400, 784)
+        self.sigmoid = nn.Sigmoid()
+
+    def encode(self, x: Variable) -> (Variable, Variable):
+        """Input vector x -> fully connected 1 -> ReLU -> (fully connected
+        21, fully connected 22)
+
+        Parameters
+        ----------
+        x : [128, 784] matrix; 128 digits of 28x28 pixels each
+
+        Returns
+        -------
+
+        (mu, logvar) : ZDIMS mean units one for each latent dimension, ZDIMS
+            variance units one for each latent dimension
+
+        """
+
+        # h1 is [128, 400]
+        h1 = self.relu(self.fc1(x))  # type: Variable
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu: Variable, log_var: Variable) -> Variable:
+        """THE REPARAMETERIZATION IDEA:
+
+        For each training sample (we get 128 batched at a time)
+
+        - take the current learned mu, stddev for each of the ZDIMS
+          dimensions and draw a random sample from that distribution
+        - the whole network is trained so that these randomly drawn
+          samples decode to output that looks like the input
+        - which will mean that the std, mu will be learned
+          *distributions* that correctly encode the inputs
+        - due to the additional KLD term (see loss_function() below)
+          the distribution will tend to unit Gaussians
+
+        Parameters
+        ----------
+        mu : [128, ZDIMS] mean matrix
+        log_var : [128, ZDIMS] variance matrix
+
+        Returns
+        -------
+
+        During training random sample from the learned ZDIMS-dimensional
+        normal distribution; during inference its mean.
+
+        """
+
+        if self.training:
+            # multiply log variance with 0.5, then in-place exponent
+            # yielding the standard deviation
+            std = log_var.mul(0.5).exp_()  # type: Variable
+            # - std.data is the [128,ZDIMS] tensor that is wrapped by std
+            # - so eps is [128,ZDIMS] with all elements drawn from a mean 0
+            #   and stddev 1 normal distribution that is 128 samples
+            #   of random ZDIMS-float vectors
+            eps = Variable(std.data.new(std.size()).normal_())
+            # - sample from a normal distribution with standard
+            #   deviation = std and mean = mu by multiplying mean 0
+            #   stddev 1 sample with desired std and mu, see
+            #   https://stats.stackexchange.com/a/16338
+            # - so we have 128 sets (the batch) of random ZDIMS-float
+            #   vectors sampled from normal distribution with learned
+            #   std and mu for the current input
+            return eps.mul(std).add_(mu)
+
+        else:
+            # During inference, we simply spit out the mean of the
+            # learned distribution for the current input.  We could
+            # use a random sample from the distribution, but mu of
+            # course has the highest probability.
+            return mu
+
+    def decode(self, z: Variable) -> Variable:
+        h3 = self.relu(self.fc3(z))
+        return self.fc4(h3)
+
+    def forward(self, x: Variable) -> (Variable, Variable, Variable):
+        mu, log_var = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, log_var)
+        return self.decode(z), mu, log_var
+
+
+def loss_function(recon_x, x, mu, logvar, batch_size) -> Variable:
+    # how well do input x and output recon_x agree?
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784))
+
+    # KLD is Kullback–Leibler divergence -- how much does one learned
+    # distribution deviate from another, in this specific case the
+    # learned distribution from the unit Gaussian
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # - D_{KL} = 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    # note the negative D_{KL} in appendix B of the paper
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    # Normalise by same number of elements as in reconstruction
+    KLD /= batch_size * 784
+
+    # BCE tries to make our reconstruction as accurate as possible
+    # KLD tries to push the distributions as close as possible to unit Gaussian
+    return BCE + KLD
