d0b6ce1775d36f2f11807bae4e6862b699fcc3dc
hlafortu <hugolafortunebrunet@hotmail.com>
2019-04-18 10:15:15 -0400

    change wgan

--- Problem_3/wgan-gp.py
+++ Problem_3/wgan-gp.py
@@ -7,9 +7,10 @@ import torch
 import torchvision.datasets
 import torchvision.transforms as transforms
 import torchvision.utils
+import torch.nn.functional as F
 
 from torch.utils.data import dataset
-from torch import nn
+from torch import nn, autograd
 # from torch.nn.modules import upsampling
 # from torch.functional import F
 from torch.optim import Adam
@@ -65,58 +66,28 @@ def get_data_loader(dataset_location, batch_size):
 # Such as no batch norm.
 
 class Critic(nn.Module):
-    def __init__(self):
+    def __init__(self, image_shape=(3, 32, 32), dim_factor=64):
         super(Critic, self).__init__()
-        self.conv_stack = nn.Sequential(
-            nn.Conv2d(3, 8, 3, padding=1),
-            #nn.BatchNorm2d(8),
-            nn.LeakyReLU(0.2),
-            
-            nn.Dropout2d(p=0.1),
-            nn.Conv2d(8, 16, 3, padding=1, stride=2),
-            #nn.BatchNorm2d(16),
-            nn.LeakyReLU(0.2),
-            
+        C, H, W = image_shape
+        assert H % 2**3 == 0, "Image height %i not compatible with architecture" % H
+        H_out = int(H / 2**3)  # divide by 2^3 bc 3 convs with stride 2
+        assert W % 2**3 == 0, "Image width %i not compatible with architecture" % W
+        W_out = int(W / 2**3)  # divide by 2^3 bc 3 convs with stride 2
+        self.pad = nn.ZeroPad2d(2)
+        self.conv1 = nn.Conv2d(C, dim_factor, 5, stride=2)
+        self.conv2 = nn.Conv2d(dim_factor, 2 * dim_factor, 5,
+                               stride=2)
+        self.conv3 = nn.Conv2d(2 * dim_factor, 4 * dim_factor, 5,
+                               stride=2)
+        self.linear = nn.Linear(4 * dim_factor * H_out * W_out, 1)
 
-            nn.Conv2d(16, 16, 3, padding=1),
-            #nn.BatchNorm2d(16),
-            nn.LeakyReLU(0.2),
-            
-
-            nn.Dropout2d(p=0.1),
-            nn.Conv2d(16, 32, 3, padding=1, stride=2),
-            #nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.2),
-            
-
-            nn.Conv2d(32, 64, 3, padding=1),
-            #nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2),
-            
-
-            nn.Dropout2d(p=0.1),
-            nn.Conv2d(64, 128, 3, padding=1, stride=2),
-            #nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2), #As in the paper https://arxiv.org/pdf/1511.06434.pdf
-
-            nn.Conv2d(128, 512, 2),
-        )
-
-        self.mlp = nn.Sequential(
-            nn.LeakyReLU(0.2),
-            #nn.Dropout(0.5),
-            nn.Linear(512, 1), #We just want to know if the image is fake or not.
-        )
-
-        init_weights(self)
-
-        # for p in self.parameters():
-        #     if p.dim() > 1:
-        #         nn.init.xavier_uniform_(p)
-
-    def forward(self, x):
-        x  = self.mlp(self.extract_features(x))
-        return x 
+    def forward(self, X):
+        H1 = F.leaky_relu(self.conv1(self.pad(X)), negative_slope=0.2)
+        H2 = F.leaky_relu(self.conv2(self.pad(H1)), negative_slope=0.2)
+        H3 = F.leaky_relu(self.conv3(self.pad(H2)), negative_slope=0.2)
+        H3_resh = H3.view(H3.size(0), -1)  # reshape for linear layer
+        disc_out = self.linear(H3_resh)
+        return disc_out
 
     def extract_features(self, x):
         return self.conv_stack(x)[:, :, 0, 0]
@@ -138,104 +109,26 @@ class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
         # Step 1, project to 1024x4x4
-        self.conv1 = nn.ConvTranspose2d(100,1024,4,1)
-        self.bn1 = nn.BatchNorm2d(1024)
-        self.relu1 = nn.ReLU()
 
-            #Step 2, project to 8x8 x 512
-        self.conv2 = nn.ConvTranspose2d(1024,512,5)
-        self.relu2 =     nn.ReLU()
-        self.bn2 =     nn.BatchNorm2d(512)
-
-            #Step 3, stride of 2 to projet 8x8 to 16x16
-        self.conv3=    nn.ConvTranspose2d(512, 256, 4, stride=2, padding=1)
-        self.relu3=    nn.ReLU()
-        self.bn3 =    nn.BatchNorm2d(256)
-
-            #Step 4, stride of 2 to projet 16x16 to 32x32
-        self.conv4 =    nn.ConvTranspose2d(256, 3, 4, stride=2, padding=1)
-        self.tanh4 =    nn.Tanh()
-
-        init_weights(self)
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(100, 1024, kernel_size=4, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(1024, 512, kernel_size=5, stride=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(256, 3, kernel_size=4, stride=2, padding=1),
+            nn.Tanh()
+        )
 
     def forward(self, x):
         #x  = self.conv_transpose_stack(x) [:, :, 0, 0]
-        x = self.conv1(x) # Step 1, project to 1024x4x4
-        x = self.bn1(x)
-        x = self.relu1(x)
-
-        x = self.conv2(x) #Step 2, project to 8x8 x 512
-        x = self.bn2(x)
-        x = self.relu2(x)
-
-        x = self.conv3(x) #Step 3, stride of 2 to projet 8x8 to 16x16
-        x = self.bn3(x)
-        x = self.relu3(x)
-
-        x = self.conv4(x) #Step 4, stride of 2 to projet 16x16 to 32x32
-        x = self.tanh4(x)
+        x = self.decoder(x)
 
         return x 
 
 
-#Designed to remove checkboard artifact https://distill.pub/2016/deconv-checkerboard/
-
-class GeneratorV2(nn.Module):
-
-    def __init__(self):
-        super(GeneratorV2, self).__init__()
-        # Step 1, project to 1024x4x4
-        self.conv1 = nn.ConvTranspose2d(100,1024,4,1)
-        self.bn1 = nn.BatchNorm2d(1024)
-        self.relu1 = nn.ReLU()
-
-            #Step 2, project to 8x8 x 512
-        self.us2 = nn.Upsample((8,8))
-        self.conv2 = nn.ConvTranspose2d(1024,512,3, stride=1, padding=1)
-        self.relu2 =     nn.ReLU()
-        self.bn2 =     nn.BatchNorm2d(512)
-
-            #Step 3, stride of 2 to projet 8x8 to 16x16
-        self.us3 = nn.Upsample((16,16))
-        self.conv3=    nn.ConvTranspose2d(512, 256, 3, stride=1, padding=1)
-        self.relu3=    nn.ReLU()
-        self.bn3 =    nn.BatchNorm2d(256)
-
-            #Step 4, stride of 2 to projet 16x16 to 32x32
-        self.us4 = nn.Upsample((32,32))
-        self.conv4 =    nn.ConvTranspose2d(256, 3, 3, stride=1, padding=1)
-        self.tanh4 =    nn.Tanh()
-
-        init_weights(self)
-
-    def forward(self, x):
-        #x  = self.conv_transpose_stack(x) [:, :, 0, 0]
-        x = self.conv1(x) # Step 1, project to 1024x4x4
-        x = self.bn1(x)
-        x = self.relu1(x)
-
-        x = self.us2(x)
-        x = self.conv2(x) #Step 2, project to 8x8 x 512
-        x = self.bn2(x)
-        x = self.relu2(x)
-
-        x = self.us3(x)
-        x = self.conv3(x) #Step 3, stride of 2 to projet 8x8 to 16x16
-        x = self.bn3(x)
-        x = self.relu3(x)
-
-        x = self.us4(x)
-        x = self.conv4(x) #Step 4, stride of 2 to projet 16x16 to 32x32
-        x = self.tanh4(x)
-
-        return x 
-
-#https://arxiv.org/pdf/1511.06434.pdf
 # All weights were initialized from a zero-centered Normal distributionwith standard deviation 0.02
-def init_weights(m):
-    if type(m) == nn.Linear:
-        torch.nn.init.normal_(m.weight, mean=0, std=0.02)
-        m.bias.data.fill_(0)
 
 #batch_size=64
 
@@ -243,39 +136,50 @@ def init_weights(m):
 def compute_gp(true_picture, fake_pictures, critic):
     cuda = torch.cuda.is_available()
     epsilon = torch.rand(true_picture.shape[0],1,1,1).uniform_(0, 1)
-    gradient_outputs = torch.ones((true_picture.shape[0],1))
+
     ones_epsilon = torch.ones((true_picture.shape[0],1,1,1))
 
     if cuda:
         epsilon = epsilon.cuda()
-        gradient_outputs = gradient_outputs.cuda()
         ones_epsilon = ones_epsilon.cuda()
 
     merged_pictures = epsilon*true_picture + ((ones_epsilon-epsilon)*fake_pictures)
+    merged_pictures = autograd.Variable(merged_pictures, requires_grad=True)
+
     out_merged = critic(merged_pictures)
-    
-    gradients = torch.autograd.grad(out_merged, merged_pictures, gradient_outputs, create_graph=True)
-    saved_gradient = gradients[0]
-    gp = torch.pow((saved_gradient.view(64,-1).norm(dim=1) -1),2).mean()
+
+    gradient_outputs = torch.ones(out_merged.size())
+
+    if cuda:
+        gradient_outputs = gradient_outputs.cuda()
+
+    gradients = torch.autograd.grad(outputs=out_merged.sum(), inputs=merged_pictures,
+                                    create_graph=True)[0]
+
+    gradients = gradients.contiguous().view(gradients.size(0), -1)
+
+    gp = torch.pow((gradients.norm(2, dim=1) -1),2).mean()
+
     return gp
 
 # Training of WGAN-GP
+
 
 def train_wgan():
     gp_scaling = 10 #Lambda in the paper
     parser = argparse.ArgumentParser(
         description='Run a wgan-gp with parameters')
-    parser.add_argument('--start_iteration', type=int, default=0,
+    parser.add_argument('--start_iteration', type=int, default=97,
                         help='Iteration number. If more than 0, it will load a saved critic/generator pair.')
     parser.add_argument('--lr_generator', type=float, default=0.00005,
                         help='Generator Learning Rate')
-    parser.add_argument('--lr_critic', type=float, default=0.0001,
+    parser.add_argument('--lr_critic', type=float, default=0.00005,
                         help='Generator Learning Rate')
-    parser.add_argument('--n_critic', type=float, default=20,   #5 in the paper, but the TA told us that we might need to tweak this one.
+    parser.add_argument('--n_critic', type=float, default=5,   #5 in the paper, but the TA told us that we might need to tweak this one.
                         help='Number of critic iterations')
     parser.add_argument('--gp_scaling', type=float, default=10,  #10 in the paper
                         help='Gradient Penalty Constant (Lambda)')
-    parser.add_argument('--max_iteration', type=int, default=100,  
+    parser.add_argument('--max_iteration', type=int, default=200,
                         help='Iteration when to stop')
     parser.add_argument('--suffix', type=str, default="default",  
                         help='id for the test run')
@@ -292,12 +196,12 @@ def train_wgan():
 
     train, valid, test = get_data_loader("svhn", batch_size)
     critic = Critic()
-    generator = GeneratorV2()
+    generator = Generator()
     params_critic = critic.parameters()
-    optimizer_critic = Adam(params_critic,lr=args.lr_critic, betas=(0.0,0.9))
+    optimizer_critic = Adam(params_critic,lr=args.lr_critic, betas=(0.5,0.9))
 
     params_generator = generator.parameters()
-    optimizer_generator = Adam(params_generator, lr=args.lr_generator, betas=(0.0,0.9))
+    optimizer_generator = Adam(params_generator, lr=args.lr_generator, betas=(0.5,0.9))
 
     cuda = torch.cuda.is_available()
     if cuda:
@@ -334,6 +238,9 @@ def train_wgan():
             #Player 1: The discriminator plays the game
             #where it wants to tell a generated sample appart from a true sample.
             time.sleep(0.1)
+
+            for p in critic.parameters():
+                p.requires_grad = True  # to avoid computation
             
             z = torch.FloatTensor(true_pictures.shape[0],100,1,1).normal_(0,1)
             
@@ -347,7 +254,7 @@ def train_wgan():
             critic.zero_grad()
             generator.zero_grad()
 
-            out_fake = critic(fake_pictures)
+            out_fake = critic(fake_pictures.detach())
             out_real = critic(true_pictures)
             wd = out_real.mean() - out_fake.mean() 
             gp = compute_gp(true_pictures, fake_pictures, critic)
@@ -361,8 +268,14 @@ def train_wgan():
             
             D_real = out_real.mean().item()
             D_fake1 = out_fake.mean().item()
+
+            if (i % 501) == 0:
+                n_critic = 100
             
             if (i%n_critic)==0:
+                n_critic = args.n_critic
+                for p in critic.parameters():
+                    p.requires_grad = False  # to avoid computation
                 generator.zero_grad()
 
                 fake_pictures = generator(z)
@@ -391,7 +304,8 @@ def train_wgan():
         if not args.no_save:
             torch.save(critic.state_dict(), "critic"+ suffix + str(epoch)+ ".pt")
             torch.save(generator.state_dict(), "generator" +suffix + str(epoch)+ ".pt")
-        torchvision.utils.save_image(fake_pictures.detach().cpu(), 'out' + suffix + str(epoch) +'.png', range=(-1,1))
+
+        torchvision.utils.save_image(fake_pictures.detach().cpu(), 'out' + suffix + str(epoch) +'.png', normalize=True)
         #torchvision.utils.save_image(fake_pictures.detach().cpu(), 'out' + suffix + 'actual_'+ str(image)+'.png', range=(-1,1))
            
 
